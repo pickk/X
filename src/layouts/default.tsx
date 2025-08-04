69f1c8edf25c961bad24d4ff688db5436594a511
pp <ppdwwww@gmail.com>
2025-08-04 23:44:58 +0800

    1

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,6 @@
 import { Link } from "@heroui/link";
 
-import { Navbar } from "@/components/navbar";
+import { MyNavbar } from "@/components/navbar";
 
 export default function DefaultLayout({
   children,
@@ -9,7 +9,7 @@ export default function DefaultLayout({
 }) {
   return (
     <div className="relative flex flex-col h-screen">
-      <Navbar />
+      <MyNavbar />
       <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
         {children}
       </main>
