cccfaf2b7e499e2353ab2f37b8c1e965a1a3bc72
pp <ppdwwww@gmail.com>
2025-08-04 23:12:56 +0800

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import { Link } from "@heroui/link";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { button as buttonStyles } from "@heroui/theme";
+import { siteConfig } from "@/config/site";
+import { title } from "@/components/primitives";
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <span className={title()}>Welcome To&nbsp;</span>
+          <span className={title({ size: "lg", color: "violet" })}>China&nbsp;</span>
+        </div>
+
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.docs}
+          >
+            Documentation
+          </Link>
+        </div>
+
+        <div className="mt-8">
+          <Snippet hideCopyButton hideSymbol variant="bordered">
+            <span>
+              Get started by editing{" "}
+              <Code color="primary">pages/index.tsx</Code>
+            </span>
+          </Snippet>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
