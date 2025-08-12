4710d685ed17413f41048e62c0e4caba05786f1c
pp <ppdwwww@gmail.com>
2025-08-12 21:15:11 +0800

    1

---
+++ src/components/cube.tsx
@@ -0,0 +1,22 @@
+import {title} from "@/components/primitives.ts";
+
+const TranslucentBox = () => {
+    return (
+        <div style={{
+            width: "600px",
+            height: "300px",
+            background: "rgba(254,254,254,0.5)",  // 半透明黑色
+            color: "#000000",                     // 文字颜色
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "16px",              // 圆角，可选
+            fontSize: "24px"
+        }}>
+            <div className={title({ className: "relative z-[99999]"})} style={{fontSize: "65px"}}>MAKE LIFE AN ADVENTURE{'\n'}
+                <div style={{ color: "rgba(1,1,1,0.89)", fontSize: "33px"}}>Welcome to China</div></div>
+        </div>
+    );
+};
+
+export default TranslucentBox;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,8 @@
-import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-import { button as buttonStyles } from "@heroui/theme";
-import { siteConfig } from "@/config/site";
-import { title } from "@/components/primitives";
+
 import StickyScrollLock from "@/components/StickyScrollLock.tsx";
 import '/src/styles/fixed-y.css';
 import MyNavbar from "@/components/navbar.tsx";
+import TranslucentBox from "@/components/cube.tsx";
 
 export default function IndexPage() {
   return (
@@ -14,33 +10,11 @@ export default function IndexPage() {
           <MyNavbar />
       <StickyScrollLock/>
       <section className="fixed-y">
-
         <div className="inline-block max-w-lg text-center justify-center">
-            <span className={title({ className: "relative z-[99999]" })}>Welcome to&nbsp;</span>
-          <span className={title({ size: "lg", color: "violet" })}>China&nbsp;</span>
-        </div>
-        <div className="flex gap-3">
-          <Link
-            isExternal
-            className={buttonStyles({
-                color: "primary",
-              radius: "full",
-              variant: "shadow",
-            })}
-            href={siteConfig.links.docs}
-          >
-            Documentation
-          </Link>
+            <TranslucentBox/>
         </div>
 
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
+
       </section>
         <div style={{ position: 'relative', top: 12322 }}>123312132123</div>
 
