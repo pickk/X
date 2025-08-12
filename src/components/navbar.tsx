3093e99f7fc4c25422b41d8fe2652a2b4ba0c6d5
pp <ppdwwww@gmail.com>
2025-08-13 00:21:49 +0800

    1

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,41 +11,41 @@ import '../styles/acme.css';
 import '../styles/Navbara.css';
 
 export default function MyNavbara() {
-
-
     return (
-        <Navbar className="navbar">
+        <div className="navbar-row">
+            <Navbar className="navbar">
             <NavbarBrand>
                 <p className="acme">ELDO'S</p>
             </NavbarBrand>
-            <NavbarContent justify="center">
-                <NavbarItem>
-                    <Link className="feature-btn1" href="#">
-                        Features
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link className="feature-now" aria-current="page" href="#">
-                        Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link className="feature-btn2" href="#">
-                        Integrations
-                    </Link>
-                </NavbarItem>
-
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
         </Navbar>
+            <Navbar className="navbar2">
+                <NavbarContent justify="end">
+                    <NavbarItem>
+                        <Link className="feature-btn1" href="#">
+                            Features
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem isActive>
+                        <Link className="feature-now" aria-current="page" href="#">
+                            Customers
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link className="feature-btn2" href="#">
+                            Integrations
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem className="hidden lg:flex">
+                        <Link href="#">Login</Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Button as={Link} color="primary" href="#" variant="flat">
+                            Sign Up
+                        </Button>
+                    </NavbarItem>
+                </NavbarContent>
+            </Navbar>
+        </div>
+
     );
 }
